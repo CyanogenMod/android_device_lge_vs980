df22be5526488a4603ac8a134b463875c1230710
Erik Hardesty <dalingrin@gmail.com>
2013-10-12 02:04:39 +0100

    vs980: Initial commit

---
+++ Android.mk
@@ -0,0 +1,7 @@
+ifneq ($(filter vs980,$(TARGET_DEVICE)),)
+
+LOCAL_PATH := $(call my-dir)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
+endif
---
+++ cm.mk
@@ -0,0 +1,26 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/cdma.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/vs980/vs980.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := vs980
+PRODUCT_NAME := cm_vs980
+PRODUCT_BRAND := LGE
+PRODUCT_MODEL := LG-VS980
+PRODUCT_MANUFACTURER := lge
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=lge/g2_open_com/g2:4.2.2/JDQ39B/D80210a.1378316352:user/release-keys PRIVATE_BUILD_DESC="g2_open_com-user 4.2.2 JDQ39B D80210a.1378316352 release-keys"
+
+PRODUCT_PACKAGES += Torch
