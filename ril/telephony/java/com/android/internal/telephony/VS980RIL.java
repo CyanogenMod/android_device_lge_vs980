74bc44b74224bb28c129e6195f63157cbd40f2b3
Ethan Chen <intervigil@gmail.com>
2016-02-07 17:12:54 -0500

    ril: Add instanceId to constructor
    NIGHTLIES-2473
    
    Change-Id: I25a45b964a7bf73b1e792fa307beae2b58e4e9dc

--- ril/telephony/java/com/android/internal/telephony/VS980RIL.java
+++ ril/telephony/java/com/android/internal/telephony/VS980RIL.java
@@ -32,11 +32,11 @@ public class VS980RIL extends LgeLteRIL implements CommandsInterface {
 
     public VS980RIL(Context context, int preferredNetworkType,
             int cdmaSubscription, Integer instanceId) {
-        this(context, preferredNetworkType, cdmaSubscription);
+        super(context, preferredNetworkType, cdmaSubscription, instanceId);
     }
 
     public VS980RIL(Context context, int networkMode, int cdmaSubscription) {
-        super(context, networkMode, cdmaSubscription);
+        this(context, networkMode, cdmaSubscription, null);
     }
 
     @Override
